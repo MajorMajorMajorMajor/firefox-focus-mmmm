81a021d1c1b32fc3b399c7a6368b79d685bc029b
Stas <stas@local>
2026-04-14 10:27:24 -0400

    topsites: remove shortcuts cap

--- mobile/android/android-components/components/feature/top-sites/src/main/java/mozilla/components/feature/top/sites/TopSitesStorage.kt
+++ mobile/android/android-components/components/feature/top-sites/src/main/java/mozilla/components/feature/top/sites/TopSitesStorage.kt
@@ -48,6 +48,8 @@ interface TopSitesStorage : Observable<TopSitesStorage.Observer> {
 
     fun reorderTopSites(topSites: List<TopSite>) {}
 
+    fun reorderTopSites(topSites: List<TopSite>) {}
+
     /**
      * Return a unified list of top sites based on the given number of sites desired.
      * If `frecencyConfig` is specified, fill in any missing top sites with frecent top site results.
--- mobile/android/android-components/components/feature/top-sites/src/main/java/mozilla/components/feature/top/sites/TopSitesUseCases.kt
+++ mobile/android/android-components/components/feature/top-sites/src/main/java/mozilla/components/feature/top/sites/TopSitesUseCases.kt
@@ -55,6 +55,12 @@ class TopSitesUseCases(topSitesStorage: TopSitesStorage) {
         }
     }
 
+    class ReorderTopSitesUseCase internal constructor(private val storage: TopSitesStorage) {
+        operator fun invoke(topSites: List<TopSite>) {
+            storage.reorderTopSites(topSites)
+        }
+    }
+
     val addPinnedSites: AddPinnedSiteUseCase by lazy {
         AddPinnedSiteUseCase(topSitesStorage)
     }
