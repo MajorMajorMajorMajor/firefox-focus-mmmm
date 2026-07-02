434c554a018d7121f583660c1cf29c6c65627400
Pi <pi@localhost>
2026-07-01 22:29:49 -0400

    focus: custom Focus patch stack

--- mobile/android/android-components/components/feature/top-sites/src/main/java/mozilla/components/feature/top/sites/PinnedSiteStorage.kt
+++ mobile/android/android-components/components/feature/top-sites/src/main/java/mozilla/components/feature/top/sites/PinnedSiteStorage.kt
@@ -96,6 +96,13 @@ class PinnedSiteStorage(context: Context) {
     }
 
     /**
+     * Replaces all pinned sites with [sites] in the given order.
+     */
+    suspend fun reorderPinnedSites(sites: List<TopSite>) = withContext(IO) {
+        pinnedSiteDao.reorderPinnedSites(sites.map { it.toPinnedSite() })
+    }
+
+    /**
      * Returns a count of pinned sites.
      */
     suspend fun getPinnedSitesCount(): Int = withContext(IO) {
